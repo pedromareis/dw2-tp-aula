21034357fddf4ed3bb1e09c2a14f7baad70a01b9
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-04-03 10:23:05 +0100

    Mudei outravez

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Diogo Fonseca <code>src/App.js</code> and save to reload.
+          <code>Diogo Fonseca</code>
         </p>
         <a
           className="App-link"
