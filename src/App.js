b0d31d2c871961fc9ed2ba0ffcfd9af142f5e8b1
luish <luismariohernandezsantos@gmail.com>
2024-04-03 12:43:47 +0100

    aula

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ListaNotas from './components/ListaNotas';
 
 function App() {
+  const notas = [
+    { id: 1, disciplina: "DW-1", nota: 16 },
+    { id: 2, disciplina: "DW-2", nota: 17 },
+    { id: 3, disciplina: "FP", nota: 13 },
+  ];
+  const notas2 = [
+    { id: 1, disciplina : "DW-1", nota: 16 },
+    { id: 2, disciplina: "DW-2", nota: 17 },
+    { id: 3, disciplina: "FP", nota: 13 },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           <code>Luis Hernandez</code>
         </p>
-        
+        <p>Aluno 1:</p>
+        <ListaNotas notas={notas} />
+        <br/>
+        <p>Aluno 2:</p>
+        <ListaNotas notas={notas2} />
+
       </header>
     </div>
   );
---
+++ src/components/ListaNotas.jsx
@@ -0,0 +1,13 @@
+import Nota from "./Nota";
+
+
+const ListaNotas = ({ notas }) => {
+    console.log(notas);
+
+    return notas.map((nota) => 
+        <Nota nota = {nota}/>
+    );
+    //  <p>{JSON.stringify(notas)}</p>
+}
+export default ListaNotas;
+// function ListaNotas(){}
