878ea580942b2ff75c1c95857456f7959a33f49f
guilhermefbgois <guigois2005@gmail.com>
2024-04-03 12:49:10 +0100

    Aula

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import ListaNotas from './components/ListaNotas';
 
 function App() {
+  const notas = [
+    {id: 1, disciplina: 'DW-1', nota: '16'},
+    {id: 2, disciplina: 'DW-2', nota: '17'},
+    {id: 3, disciplina: 'FP', nota: '13'},
+  ];
+
+  const notas2 = [
+    {id: 1, disciplina: 'DW-1', nota: '19'},
+    {id: 2, disciplina: 'DW-2', nota: '7'},
+    {id: 3, disciplina: 'FP', nota: '1'},
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>Guilherme Góis</code>
         </p>
+        <code>Aluno 1:</code>
+          <ListaNotas notas = {notas}/>
+          <p></p>
+        <code>Aluno 2:</code>
+          <ListaNotas notas = {notas2}/>
       </header>
     </div>
   );
