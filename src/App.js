fee46adb0b25d45ba10b6c5cb31a8967fd3e80d7
Joao Belo <jobelo@deloitte.pt>
2024-04-03 13:54:47 +0100

    Aula 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { ListaNotas } from './components/ListaNotas';
 
 function App() {
+  const aluno1 = [
+    {id: 1, disciplina: 'DW-1', nota: 11},
+    {id: 2, disciplina: 'DW-2', nota: 13},
+    {id: 3, disciplina: 'FP', nota: 16}
+  ];
+  
+  const aluno2 = [
+    {id: 1, disciplina: 'DW-1', nota: 15},
+    {id: 2, disciplina: 'DW-2', nota: 17},
+    {id: 3, disciplina: 'FP', nota: 20}
+  ];
+
+
+  let soma = 20;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,9 @@ function App() {
         <p>
           <code>Joao Belo</code>
         </p>
+      <ListaNotas notas={aluno1} testeVariavel={"Teste"} numero={soma}></ListaNotas>
+      <br />
+      <ListaNotas notas={aluno2} testeVariavel={"Teste"} numero={soma}></ListaNotas>
       </header>
     </div>
   );
---
+++ src/components/ListaNotas.jsx
@@ -0,0 +1,11 @@
+import Nota from "./Nota";
+export const ListaNotas = ({notas, testeVariavel, numero}) => {
+    console.log(notas);
+    //console.log(testeVariavel);
+    //console.log(numero);
+
+    return notas.map((nota) => <Nota nota={nota} />);
+    //return <p>{JSON.stringify(notas)}</p>
+};
+
+export default ListaNotas;
