b964421051126ca03a51a5b16c13ba668aa8b443
luish <luismariohernandezsantos@gmail.com>
2024-04-03 10:22:44 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Luis hdez</code> and save to reload.
+          <code>Luis Hernandez</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          fixeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        </a>
+        
       </header>
     </div>
   );
