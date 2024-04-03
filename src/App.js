03b464337f7894d9f3a73897f9918ed84d13e8cc
Simão <simaobaenevesne1@gmail.com>
2024-04-03 12:51:33 -0400

    Trabalho aula

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-// simao
+import ListaNotas from './components/listanotas';
+import Nota from './components/Nota';
+
 function App() {
+  const alunos1= [
+    {id: 1, disciplina: "DW-1", nota: 16},
+    {id: 2, disciplina: "DW-2", nota: 17},
+    {id: 3, disciplina: "FP", nota: 13},
+  ];
+  const alunos2= [
+    {id: 1, disciplina: "DW-1", nota: 12},
+    {id: 2, disciplina: "DW-2", nota: 15},
+    {id: 3, disciplina: "FP", nota: 17},
+  ];
+
+let soma=20;
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Simão Neves</p>
+      <p>Aluno 1</p>
+      <ListaNotas notas={alunos1} />
+      <p>Aluno 2</p>
+      <ListaNotas notas={alunos2} />
       </header>
+      
+
+      
     </div>
   );
 }
---
+++ src/components/Nota.jsx
@@ -0,0 +1,5 @@
+const Nota = ({ nota }) => {
+    return <div><p>id = {nota.id}</p><p>{nota.nota}</p></div>;
+}
+
+export default Nota
