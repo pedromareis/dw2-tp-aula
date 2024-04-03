204cb452d9169f1395f4098978d0ec6d0273a1c3
david <dmneto2005@gmail.com>
2024-04-03 12:42:31 +0100

    5%!!!!!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ListaNotas from "./components/ListaNotas";
 
 function App() {
+  const aluno1 = [
+    { id: 1, disciplina: "DW-1", nota: 16 },
+    { id: 2, disciplina: "DW-2", nota: 17 },
+    { id: 3, disciplina: "FP", nota: 13 },
+  ];
+
+  const aluno2 = [
+    { id: 1, disciplina: "DW-1", nota: 16 },
+    { id: 2, disciplina: "DW-2", nota: 17 },
+    { id: 3, disciplina: "FP", nota: 13 },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,10 @@ function App() {
         <p>
           <code>david.</code>
         </p>
+        <p>Aluno 1:</p>
+        <ListaNotas notas={aluno1} />
+        <p>Aluno 2:</p>
+        <ListaNotas notas={aluno2} />
       </header>
     </div>
   );
