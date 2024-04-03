72ed3eee037acd1e96a0d17c27f3e95f48487673
Brito <jcbrito.2005@gmail.com>
2024-04-03 12:51:43 +0100

    Trabalho aula 3-4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ListaNotas from"./components/ListaNotas";
+
 
 function App() {
+  const aluno1 = [
+    {id: 1, disciplina: 'DW-1', nota: 16},
+    {id: 2, disciplina: 'DW-2', nota: 17},
+    {id: 3, disciplina: 'FP', nota: 11},
+  ];
+
+  const aluno2 = [
+    {id: 4, disciplina: 'DW-1', nota: 13},
+    {id: 5, disciplina: 'DW-2', nota: 5},
+    {id: 6, disciplina: 'FP', nota: 19},
+  ];
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Joao Brito
-        </p>
+
+          <code>João Brito</code>
         
+        <p>Aluno 1:</p>
+        <ListaNotas notas = {aluno1}/> 
+        <br />
+        <p>Aluno 2:</p>
+        <ListaNotas notas = {aluno2}/> 
+
+        </p>
       </header>
     </div>
   );
