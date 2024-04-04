c2a2dc2011a2163456133943bb2fb3517fafd5f1
Pedro Reis <pedro.reis@theloop.pt>
2024-04-04 12:01:30 +0100

    Mocked data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import ListaNotas from "./components/ListaNotas";
+import { aluno1 } from "./data/notas-alunos";
 
 function App() {
-  const aluno1 = [
-    { id: 1, disciplina: "DW-1", nota: 16 },
-    { id: 2, disciplina: "DW-2", nota: 17 },
-    { id: 3, disciplina: "FP", nota: 13 },
-  ];
-
   return (
     <div className="App">
       <header className="App-header">
