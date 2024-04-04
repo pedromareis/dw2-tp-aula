3f8a4132a133e524df1f74814e7a73841da761db
Correia <martimdcorreia@deloitte.pt>
2024-04-04 12:03:13 +0100

    Segunda aula

--- src/components/Nota.jsx
+++ src/components/Nota.jsx
@@ -1,5 +1,16 @@
-const Nota = ({nota}) => {
-    return <p>{nota.disciplina + ': ' + nota.nota}</p>;
-};
- 
-export default Nota;
+const Nota = ({ nota, funcao, estilos }) => {
+    return (
+      <div
+      style={{...estilos, cursor: "pointer", backgroundColor: "yellow" }}
+        onClick={() => {
+          console.log("Cliquei no elemento no ficheiro Nota.jsx");
+          funcao(nota.disciplina);
+        }}
+      >
+        {nota.disciplina + ": " + nota.nota + " valores"}
+      </div>
+    );
+  };
+   
+  export default Nota;
+   
