e4dd48587f66626c4c0fbb65283783c329131958
Correia <martimdcorreia@deloitte.pt>
2024-04-04 09:39:40 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Pedro Reis</code>
+          <code>Martim Correia</code>
         </p>
       </header>
     </div>
