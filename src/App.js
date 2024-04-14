633f206848334c19c9a96543e4c99d79a260c90c
Pedro Reis <pedro.reis@theloop.pt>
2024-04-14 17:08:17 +0100

    Teste Supabase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  "https://ulplsaiimovuibgacewe.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVscGxzYWlpbW92dWliZ2FjZXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxMDk5OTAsImV4cCI6MjAyODY4NTk5MH0.QWejC96bJ5bQ5VxQMRBqdKS90Ri8m5CDi--PfYOmPWE"
+);
 
 function App() {
   const [numero, setNumero] = useState(0);
   const [numero2, setNumero2] = useState(0);
   const [alerta, setAlerta] = useState(undefined);
+
+  const [casas, setCasas] = useState([]);
 
   useEffect(() => {
     console.log("useEffect tipo 1: sem dependências");
@@ -12,6 +19,15 @@ function App() {
 
   useEffect(() => {
     console.log("useEffect tipo 2: array dependências vazio");
+  }, []);
+
+  async function getCasas() {
+    const { data } = await supabase.from("casas").select();
+    setCasas(data);
+  }
+
+  useEffect(() => {
+    getCasas();
   }, []);
 
   useEffect(() => {
@@ -38,6 +54,15 @@ function App() {
           Incrementa o n2
         </button>
         {alerta && <p>{alerta}</p>}
+        <ul>
+          {casas &&
+            casas.map((casa) => (
+              <>
+                <li key={casa.nome}>{casa.nome}</li>
+                <li key={casa.preco}>{casa.preco}</li>
+              </>
+            ))}
+        </ul>
       </header>
     </div>
   );
