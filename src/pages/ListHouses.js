5cc5db1a8fc91d8ad5cd78c33bc9a8df2c2178c8
Pedro Reis <pedro.reis@theloop.pt>
2024-05-12 18:12:30 +0100

    Fixed map when Supabase is down

--- src/pages/ListHouses.js
+++ src/pages/ListHouses.js
@@ -33,52 +33,55 @@ const ListHouses = ({ supabase }) => {
           justifyContent: "center",
         }}
       >
-        {casas.map((casa) => (
-          <div
-            key={casa.id}
-            style={{
-              backgroundColor: "#fafafa",
-              borderRadius: 8,
-              padding: "32px 16px",
-              width: 500,
-              textAlign: "center",
-            }}
-          >
-            {casa.tipo && (
-              <Tag
-                color={casa.tipo === "arrendar" ? "#f50" : "#87d068"}
-                style={{ fontSize: 24, padding: 8 }}
-              >
-                {casa.tipo}
-              </Tag>
-            )}
+        {casas &&
+          casas.map((casa) => (
+            <div
+              key={casa.id}
+              style={{
+                backgroundColor: "#fafafa",
+                borderRadius: 8,
+                padding: "32px 16px",
+                width: 500,
+                textAlign: "center",
+              }}
+            >
+              {casa.tipo && (
+                <Tag
+                  color={casa.tipo === "arrendar" ? "#f50" : "#87d068"}
+                  style={{ fontSize: 24, padding: 8 }}
+                >
+                  {casa.tipo}
+                </Tag>
+              )}
 
-            <h3>{casa.nome}</h3>
-            <p>{casa.descricao}</p>
-            <p style={{ fontSize: 20 }}>{casa.morada}</p>
-            <p style={{ fontSize: 32, fontWeight: "bold" }}>
-              {casa.preco ? casa.preco + "€" : "- €"}
-            </p>
-            <div style={{ display: "flex", gap: 8, justifyContent: "center" }}>
-              <Button
-                type="primary"
-                style={{ fontSize: 24, height: "auto", padding: 16 }}
-                icon={<EditOutlined />}
+              <h3>{casa.nome}</h3>
+              <p>{casa.descricao}</p>
+              <p style={{ fontSize: 20 }}>{casa.morada}</p>
+              <p style={{ fontSize: 32, fontWeight: "bold" }}>
+                {casa.preco ? casa.preco + "€" : "- €"}
+              </p>
+              <div
+                style={{ display: "flex", gap: 8, justifyContent: "center" }}
               >
-                <Link to={`/edit/${casa.id}`}>Editar</Link>
-              </Button>
-              <Button
-                type="primary"
-                danger
-                style={{ fontSize: 24, height: "auto", padding: 16 }}
-                icon={<DeleteOutlined />}
-                onClick={() => deleteCasa(casa.id)}
-              >
-                Apagar
-              </Button>
+                <Button
+                  type="primary"
+                  style={{ fontSize: 24, height: "auto", padding: 16 }}
+                  icon={<EditOutlined />}
+                >
+                  <Link to={`/edit/${casa.id}`}>Editar</Link>
+                </Button>
+                <Button
+                  type="primary"
+                  danger
+                  style={{ fontSize: 24, height: "auto", padding: 16 }}
+                  icon={<DeleteOutlined />}
+                  onClick={() => deleteCasa(casa.id)}
+                >
+                  Apagar
+                </Button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       <Button
